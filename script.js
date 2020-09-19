21dfd04b811d12ee398e74047f8deb90fa0a4a6f
Jacob Cowan <mrjcowman@gmail.com>
2020-09-19 14:23:35 -0700

    Built site structure and general css

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 const apiKey = "77e74a091d5990f837c233f900382ee3";
+
+$(document).foundation();
+
+// let headerHeight = $("header").outerHeight();
+// let bodyHeight = $("body").height();
+
+
+
+// if(window.matchMedia("(min-width: 39.9375em)").matches){
+//     $("main").height(bodyHeight-headerHeight);
+// }
