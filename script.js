e88138dfe4de37c79ba4799e8a1371fdcacb91a7
Jacob Cowan <mrjcowman@gmail.com>
2020-09-21 09:35:53 -0700

    Outlined todos, changed height update method name

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const apiKey = "77e74a091d5990f837c233f900382ee3";
 
 $(document).foundation();
 
+
+// Adjust the height of the main element to fill a screen when wider than small
 const smallScreenMedia = window.matchMedia("(min-width: 39.9375em)");
 let headerHeight = $("header").outerHeight();
 let bodyHeight = $("body").height();
 
-const updateSidebarHeight = ()=>{
+const updateMainHeight = ()=>{
     if(smallScreenMedia.matches){
         $("main").height(bodyHeight-headerHeight);
     } else {
@@ -14,5 +16,17 @@ const updateSidebarHeight = ()=>{
     }
 }
 
-updateSidebarHeight();
-smallScreenMedia.addEventListener("change", updateSidebarHeight);
+updateMainHeight();
+smallScreenMedia.addEventListener("change", updateMainHeight);
+
+// TODO: Process Search Query
+
+// TODO: Get information from API
+
+// TODO: Populate fields on current weather conditions
+
+// TODO: Populate cards for week's forecast
+
+// TODO: Poll API periodically for updates
+
+// TODO: Handle previous search array
